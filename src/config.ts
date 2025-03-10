2f616068f11aa2f157be75294d96ab6c7e90dc24
naxor <naxordeve@gmail.com>
2025-03-10 20:15:05 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 /**
  * Pastebin api goes here lol
  */
-export const paste_api_dev_key = " ";
+export const paste_dev_api_key = " ";
 export const auth = '../session/creds.json';
-export const api_user_name = ""; //paste username 
+export const api_user_username = ""; //paste username 
 export const api_user_password = ""; //paste password
